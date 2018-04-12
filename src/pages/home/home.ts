b4841c5562089cc330dccfa94977f808071d32ab
djello2nice <37280635+djello2nice@users.noreply.github.com>
2018-04-11 21:16:24 -0400

    WIP

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { RegisterPage } from '../register/register';
+
 
 @Component({
   selector: 'page-home',
@@ -9,6 +11,9 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+  nextPage(){
+    this.navCtrl.push(RegisterPage);
   }
 
 }
